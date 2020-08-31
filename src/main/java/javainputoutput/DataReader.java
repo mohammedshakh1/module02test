aff78d71956a1066eb7dab6e32a5f824d4910bcb
Your Name <you@example.com>
2020-08-31 03:50:37 -0400

    added

--- src/main/java/javainputoutput/DataReader.java
+++ src/main/java/javainputoutput/DataReader.java
@@ -1,5 +1,9 @@
 package javainputoutput;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DataReader {
 
 	public static void main(String[] args) {
@@ -11,6 +15,25 @@ public class DataReader {
 		 * Use try....catch block to handle Exception.
 		 * Use ForEach loop/while loop/Iterator to retrieve data.
 		 */
+		DataReader dataReader = new DataReader();
+		//dataReader.readFile();
+
+		public void readFile()  throw  IOException, FileNotFoundException{
+
+			String filePath = "/Users/asif/QA-Automation/Auto-Project-Classwork/module02test/src/main/resources/Key-To-Success";
+
+			FileReader fileReader = new FileReader(filePath);
+
+			//fileReader.read();
+			int i;
+			while ((i = fileReader.read())!=-1){
+				//print don't create new line ...use print instead of println
+				//char so it print letter from numbers since Read class hold numbers as letter
+				System.out.print((char) i);
+
+			}
+
+		}
 
 	}
 
